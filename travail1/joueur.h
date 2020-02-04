800b88dc816f827c2593b5c7baada96ae07f79bd
morganktjs <60520279+morganktjs@users.noreply.github.com>
2020-02-04 14:16:30 -0500

    Revert "Prog class Joueur"
    This reverts commit 0ef62e3738548c1233cd35d0258653e6bac1aad4.

--- travail1/joueur.h
+++ travail1/joueur.h
@@ -8,18 +8,6 @@ using namespace std;
 const int maxCartesAJouer = 26;
 class Joueur
 {
-private:
-	string nom;
-	int nombreVictoire;
-	int nombreDefaite;
-	Carte* mainDuJoueur[26];
 
-public:
-	Joueur();
-	void SetNom(string aNom);
-	void AjouterUneCarteDansLaMain(Carte* aCarte);
-	void EnleverLesCartesDeLaMain();
-	void AugmenterVictoire();
-	void AugmenterDefaite();
 };
 #endif
