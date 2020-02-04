0b74f64186e20cb619ac69065915f7db2f888cff
morganktjs <60520279+morganktjs@users.noreply.github.com>
2020-02-04 17:12:26 -0500

    Revert "Prog class donnees"
    This reverts commit 33f4870d5b93e1de77c8041ba1dd236f3099a637.

--- travail1/donnees.cpp
+++ travail1/donnees.cpp
@@ -9,23 +9,13 @@ Donnees::Donnees()
 
 void Donnees::InitialiserCartes()
 {
+	int cpt = 0;
 
 	for (int i = 0; i < 4; i++)
 	{
 		for (int j = 0; j < 13; j++)
 		{
-			Carte newCarte;
-			switch (i)
-			{
-			case 0:
-				newCarte.InitialiserCarte(j, "coeur");
-			case 1:
-				newCarte.InitialiserCarte(j, "carreau");
-			case 2:
-				newCarte.InitialiserCarte(j, "pique");
-			case 3:
-				newCarte.InitialiserCarte(j, "trèfle");
-			}	
+
 		}
 	}
 }
