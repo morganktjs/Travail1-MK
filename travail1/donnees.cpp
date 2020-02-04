6be73d678e4c8f0dddf7948420254141a248ad1a
morganktjs <60520279+morganktjs@users.noreply.github.com>
2020-02-04 17:13:27 -0500

    Prog class donnees
    fin 2e partie

--- travail1/donnees.cpp
+++ travail1/donnees.cpp
@@ -1,4 +1,4 @@
-#include "donnees.h"
+﻿#include "donnees.h"
 
 
 Donnees::Donnees()
@@ -16,6 +16,18 @@ void Donnees::InitialiserCartes()
 		for (int j = 0; j < 13; j++)
 		{
 
+			Carte newCarte;
+			switch (i)
+			{
+			case 0:
+				newCarte.InitialiserCarte(j, "coeur");
+			case 1:
+				newCarte.InitialiserCarte(j, "carreau");
+			case 2:
+				newCarte.InitialiserCarte(j, "pique");
+			case 3:
+				newCarte.InitialiserCarte(j, "tr�fle");
+			}
 		}
 	}
 }
