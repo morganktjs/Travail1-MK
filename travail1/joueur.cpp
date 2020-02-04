0ef62e3738548c1233cd35d0258653e6bac1aad4
morganktjs <60520279+morganktjs@users.noreply.github.com>
2020-02-04 14:12:34 -0500

    Prog class Joueur
    1ère partie du travail

--- travail1/joueur.cpp
+++ travail1/joueur.cpp
@@ -1,2 +1,46 @@
 #include "joueur.h"
 
+- void Joueur::SetNom(string aNom)
+{
+	nom = aNom;
+}
+
+Joueur::Joueur()
+{
+	nom = "";
+	nombreVictoire = 0;
+	nombreDefaite = 0;
+	for (int i = 0; i < maxCartesAJouer; i++)
+	{
+		mainDuJoueur[i] = NULL;
+	}
+}
+
+void Joueur::AjouterUneCarteDansLaMain(Carte* aCarte)
+{
+	for (int i = 0; i < maxCartesAJouer; i++)
+	{
+		if (mainDuJoueur[i] == NULL)
+		{
+			mainDuJoueur[i] = aCarte;
+		}
+	}
+}
+
+void Joueur::EnleverLesCartesDeLaMain()
+{
+	for (int i = 0; i < maxCartesAJouer; i++)
+	{
+		mainDuJoueur[i] = NULL;
+	}
+}
+
+void Joueur::AugmenterDefaite()
+{
+	nombreDefaite++;
+}
+
+void Joueur::AugmenterVictoire()
+{
+	nombreVictoire++;
+}
